19c2d8030585ab5dcfc60e945a3a3fb405c77d73
S. R. Mohammad Imran <raining101290@users.noreply.github.com>
2024-07-03 18:36:53 +0600

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,44 +1,48 @@
 export const classes = [
-  {
-    id: 'Four',
-    title: 'Four',
-  },
-  {
-    id: 'Five',
-    title: 'Five',
-  },
-  {
-    id: 'Ten',
-    title: 'Ten',
-  },
-  {
-    id: 'Eleven',
-    title: 'Eleven',
-  },
+  // {
+  //   id: 'Four',
+  //   title: 'Four',
+  // },
+  // {
+  //   id: 'Five',
+  //   title: 'Five',
+  // },
+  // {
+  //   id: 'Ten',
+  //   title: 'Ten',
+  // },
+  // {
+  //   id: 'Eleven',
+  //   title: 'Eleven',
+  // },
   {
     id: 'SSC Batch',
-    title: 'S.S.C Batch',
+    title: 'S.S.C',
   },
+  // {
+  //   id: 'HSC Batch',
+  //   title: 'H.S.C Batch',
+  // },
   {
-    id: 'HSC Batch',
-    title: 'H.S.C Batch',
+    id: 'HSC 2024',
+    title: 'HSC 1st Year',
   },
   {
     id: 'HSC 2024',
-    title: 'HSC 2024',
+    title: 'HSC 2nd Year',
   },
-  {
-    id: 'S.S.C Model Test 2023',
-    title: 'S.S.C Model Test 2023',
-  },
-  {
-    id: 'H.S.C Admission Test 2022',
-    title: 'H.S.C Admission Test 2022',
-  },
-  {
-    id: 'H.S.C. Model Test 2023',
-    title: 'H.S.C. Model Test 2023',
-  },
+  // {
+  //   id: 'S.S.C Model Test 2023',
+  //   title: 'S.S.C Model Test 2023',
+  // },
+  // {
+  //   id: 'H.S.C Admission Test 2022',
+  //   title: 'H.S.C Admission Test 2022',
+  // },
+  // {
+  //   id: 'H.S.C. Model Test 2023',
+  //   title: 'H.S.C. Model Test 2023',
+  // },
 ]
 
 export const versions = [
